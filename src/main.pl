04b95b96c79eb4d6951b66fb774ee9068ebb8f72
Alexey Kovzel <alexey.kovzel@gmail.com>
2023-05-28 15:35:20 +0200

    123

--- src/main.pl
+++ src/main.pl
@@ -13,28 +13,3 @@ snake(HintsX, HintsY, Grid, Output) :-
     build_path(Ends, Dim, Hints, MoveOrder, Path),
     check_path(Path, (Empty, Body), Hints),
     convert_path(Path, Grid, Output), !.
-
-% Check if path respects hints and prefilled values
-check_path(Path, Prefilled, (HintsX, HintsY)) :-
-    check_hints(Path, 1, HintsX, 0),
-    check_hints(Path, 2, HintsY, 0),
-    check_prefilled(Path, Prefilled).
-
-check_prefilled(Path, (Empty, Body)) :-
-    maplist([BodyCell]>>(member(BodyCell, Path)), Body),
-    maplist([EmptyCell]>>(\+ member(EmptyCell, Path)), Empty).
-
-check_hints(_, _, [], _).
-check_hints(Path, Axis, [Hint|Hints], Level) :-
-    ( Hint \= -1 -> count_tuples(Path, Axis, Level, Hint); true),
-    NextLevel is Level + 1,
-    check_hints(Path, Axis, Hints, NextLevel).
-
-% Convert path to a grid
-convert_path(Path, Input, Output) :-
-    map2d((0, 0), convert_cell, Path, Input, Output).
-
-convert_cell((X, Y), Path, A, B) :-
-    A == 1 -> B = 1; 
-    member((Y, X), Path) -> B = 2; 
-    B = 0.
